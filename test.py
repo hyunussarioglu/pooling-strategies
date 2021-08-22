bf010ffe9ad3478ab47c288906101b7544f65d01
hyunussarioglu <66547525+hyunussarioglu@users.noreply.github.com>
2021-08-22 10:43:27 +0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoTokenizer
-
-tokenizer = AutoTokenizer.from_pretrained("dbmdz/bert-base-turkish-cased")
-
-text = "Bilgiye ulaşmanın en güzel yolu düşünmektir."
-
-tokenized = tokenizer.tokenize(text, add_special_tokens = True)
-
-print(tokenized)
